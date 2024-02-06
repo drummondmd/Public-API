b01dfa2bd2bbcee3c21096d41867489e39054db0
Marcelo Drummond Duarte <marceloll2@yahoo.com.br>
2024-02-06 17:29:36 -0300

    Finalizado projeto

--- index.js
+++ index.js
@@ -6,18 +6,38 @@ import bodyParser from "body-parser";
 ///const
 const port = 3000;
 const app = express();
-const API_URL = "https://api.fda.gov/drug/event.json";
-const key = "sMUYA92eegDtrgwLNdF6pWaR4v41HHBgTXWCbMM9";
+const API_URL = "https://api.fda.gov/";
+const drugEndpoint ="drug/event.json?"
+const labelEndpoint="drug/label.json?"
+const key = "api_key=sMUYA92eegDtrgwLNdF6pWaR4v41HHBgTXWCbMM9&";
 
 //middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  const result = await axios.get(
-    'https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:"rivaroxaban"&count=patient.reaction.reactionmeddrapt.exact&limit=2',
-  );
-  res.render("index.ejs", result.data);
+app.get("/",async (req,res)=>{
+  res.render("index.ejs",{data:"Nada a ser mostrado por aqui!"})
+})
+
+app.get("/resposta", async (req, res) => {
+  const substance=   req.query.med;
+
+  try{
+    const colateral = await axios.get(
+      API_URL + drugEndpoint +key+"search=patient.drug.openfda.substance_name:"+substance+"&count=patient.reaction.reactionmeddrapt.exact&limit=20",
+    );
+    const label = await axios.get(
+      API_URL + labelEndpoint +key+"search=openfda.substance_name:"+substance+"&limit=20"
+    );
+    res.render("index.ejs", {substance:substance,colateral:colateral.data,label:label.data});
+
+
+  }catch (error) {
+    console.log(error.data + error.response.status);
+    res.render("index.ejs",{data:"Um erro foi encontrado,tente novamente"})
+
+  }
+
 });
 
 app.listen(port, () => {
